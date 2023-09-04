d784e3b8b079085479a8dee3943d69a03ad6bd2c
Purnendu Talukder <ptsourav21@gmail.com>
2023-09-04 19:14:17 +0600

    jQuery

---
+++ 19-jQuery/index.js
@@ -0,0 +1,13 @@
+
+$("button").click(function(){
+    $("h1").css("color", "purple");
+})
+$("input").keypress(function(event){
+    $("h1").text(event.key);
+});
+$("h1").on("mouseover", function(){
+    $("h1").css("color", "blue");
+    setTimeout(function(){
+        $("h1").css("color", "red");
+    },1000)
+})
