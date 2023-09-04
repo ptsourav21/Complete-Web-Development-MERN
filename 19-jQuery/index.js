29ea2e99387763c863e7f5f8facced116e6fde89
Purnendu Talukder <ptsourav21@gmail.com>
2023-09-04 23:49:49 +0600

    Update index.js

--- 19-jQuery/index.js
+++ 19-jQuery/index.js
@@ -1,13 +1,12 @@
-
-$("button").click(function(){
-    $("h1").css("color", "purple");
-})
-$("input").keypress(function(event){
-    $("h1").text(event.key);
+$("button").click(function () {
+  $("h1").css("color", "purple");
 });
-$("h1").on("mouseover", function(){
-    $("h1").css("color", "blue");
-    setTimeout(function(){
-        $("h1").css("color", "red");
-    },1000)
-})
+$("input").keypress(function (event) {
+  $("h1").text(event.key);
+});
+$("h1").on("mouseover", function () {
+  $("h1").css("color", "blue");
+  setTimeout(function () {
+    $("h1").css("color", "red");
+  }, 1000);
+});
