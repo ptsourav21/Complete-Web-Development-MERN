ba80bf59532a5bae0be610bd7fa7dee427b3f919
Purnendu Talukder <ptsourav21@gmail.com>
2023-09-09 06:48:45 +0600

    view data from api

--- 25-APIs Application Programminn Interface/01-Weather Project/app.js
+++ 25-APIs Application Programminn Interface/01-Weather Project/app.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 const https = require("https");
 const app = express();
@@ -6,9 +7,19 @@ app.get("/", function (req, res) {
   const Url =
     "https://api.openweathermap.org/data/2.5/weather?q=London&appid=d316945face40dfd8707981a66f2917c&units=metric";
   https.get(Url, function (response) {
-    console.log(response);
+    console.log(response.statusCode);
+    response.on("data", function(data) {
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const desc = weatherData.weather[0].description;
+      const img = weatherData.weather[0].icon;
+
+      res.write("<p>The weather is "+desc+"</p>")
+      res.write("<h1>The temperature in London is: "+temp+" Degree<h1>");
+      res.write("<img src='https://openweathermap.org/img/wn/"+img+"@2x.png'>");
+      res.send();
+    });
   });
-  res.send("Server is up and running");
 });
 
 app.listen(3000, function () {
