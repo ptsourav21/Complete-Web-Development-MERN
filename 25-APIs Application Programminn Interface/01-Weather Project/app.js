4db0c36863adf81da4fcedecbd2af0c62c64a1a1
Purnendu Talukder <ptsourav21@gmail.com>
2023-09-09 08:08:23 +0600

    weather app project using api done

--- 25-APIs Application Programminn Interface/01-Weather Project/app.js
+++ 25-APIs Application Programminn Interface/01-Weather Project/app.js
@@ -1,22 +1,40 @@
-
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
+
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+  const city = req.body.cityName;
+  const appid = "d316945face40dfd8707981a66f2917c";
+  const unit = "metric";
   const Url =
-    "https://api.openweathermap.org/data/2.5/weather?q=London&appid=d316945face40dfd8707981a66f2917c&units=metric";
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=" +
+    appid +
+    "&units=" +
+    unit +
+    "";
   https.get(Url, function (response) {
     console.log(response.statusCode);
-    response.on("data", function(data) {
+    response.on("data", function (data) {
       const weatherData = JSON.parse(data);
       const temp = weatherData.main.temp;
       const desc = weatherData.weather[0].description;
       const img = weatherData.weather[0].icon;
 
-      res.write("<p>The weather is "+desc+"</p>")
-      res.write("<h1>The temperature in London is: "+temp+" Degree<h1>");
-      res.write("<img src='https://openweathermap.org/img/wn/"+img+"@2x.png'>");
+      res.write("<p>The weather is " + desc + "</p>");
+      res.write("<h1>The temperature in "+city+" is: " + temp + " Degree<h1>");
+      res.write(
+        "<img src='https://openweathermap.org/img/wn/" + img + "@2x.png'>"
+      );
       res.send();
     });
   });
