5a6021a66296732f9e92d1ed8352fbf81dea44bc
Purnendu Talukder <ptsourav21@gmail.com>
2023-09-15 23:55:54 +0600

    Fetch API

---
+++ 15.1-Javascript Nuggets/18-Fetch API/app.js
@@ -0,0 +1,24 @@
+//fetch api - browser api for HTTP (ajax) requests
+// default - GET requests, supports other methods as well
+// return promises
+
+const url = "https://www.course-api.com/react-tour-project";
+console.log(fetch(url));
+
+// fetch(url)
+//   .then((response) => response.json())
+//   .then((data) => console.log(data))
+//   .catch((error) => console.log(error));
+
+const getTours = async () => {
+  try {
+    const resp = await fetch(url);
+    const data = await resp.json();
+    // console.log(data);
+    return data
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+console.log(getTours());
